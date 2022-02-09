5775f5b9322d8d2cda0b2ae18e89a2e1999c7509
Venkatesh <65492179+Theunfortunateguy@users.noreply.github.com>
2022-02-09 19:53:15 +0530

    Adding solution to exercise20

---
+++ exercise20.rb
@@ -0,0 +1,25 @@
+class Interest
+    attr_accessor :principal, :time
+    RATE = 0.1
+    def initialize(principal, time)
+        @principal =principal
+        @time =time
+    end
+    def compound
+        @principal * ((1 + RATE) ** @time).to_f
+    end
+  
+    def simple
+        @principal + (@principal * @time * RATE).to_f
+    end
+  
+    def difference
+       compound-simple
+    end
+end
+puts "Enter Principal "
+principal = gets.chomp.to_f
+puts "Enter Time"
+time =gets.chomp.to_f
+final_result = Interest.new(principal,time)
+puts "Interest difference:#{(final_result.difference).round(2)}" 
