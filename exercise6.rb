ff22340dc7e7e44f1cd04fe635257d286fde96f3
Venkatesh <65492179+Theunfortunateguy@users.noreply.github.com>
2022-02-21 17:11:39 +0530

    Updating exercise6.rb

--- exercise6.rb
+++ exercise6.rb
@@ -2,26 +2,8 @@ class Vehicle
   def initialize(name, price)
         @name = name
         @price = price
-  end  
-end
-
-class Bike < Vehicle
-  $increased_price=0
-  def initialize(name,price,dealer,increased_percent)
-    @name=name
-    @price=price
-    @dealer=dealer
-    @increased_percent=increased_percent
-      changed_price(increased_percent)
   end
-      
-  def changed_price(increased_percent)
-    price_amount=Float(@price*(100+increased_percent))
-    final_price=Float(price_amount/100)
-    @final_price=final_price
-  end
-
-  def print_details
+  def to_s
     puts "\nBike name: #{@name}"
     puts "Bike price: #{@price}"
     puts "Bike dealer: #{@dealer}"
@@ -30,10 +12,34 @@ class Bike < Vehicle
     puts "Bike name: #{@name}"
     puts "Bike price: #{@final_price}"
     puts "Bike dealer: #{@dealer}"
+    exit
   end
 end
-    
-case1=Bike.new("apache" , 58000 ,"The Bike showroom" ,12.5)
-puts case1.print_details
+class Bike < Vehicle
+  $increased_price=0
+  def initialize(name,price,dealer,increased_percent)
+    @name=name
+    @price=price
+    @dealer=dealer
+    @increased_percent=increased_percent
+    changed_price(increased_percent)
+  end
+      
+  def changed_price(increased_percent)
+    price_amount=Float(@price*(100+increased_percent))
+    final_price=Float(price_amount/100)
+    @final_price=final_price
+  end
+end
 
-#end
+if ARGV.length<1
+  puts "Please provide an input"
+  exit
+else
+  name=ARGV[0]
+  price=ARGV[1].to_i
+  dealer=ARGV[2]
+  increased_percent=ARGV[3].to_i
+  test=Bike.new(name,price,dealer,increased_percent)
+  puts test
+end
