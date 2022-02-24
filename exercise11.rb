e6501ef67b358267b75681ed91bcb86621f3b1a3
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:30:08 +0530

    updating

--- exercise11.rb
+++ exercise11.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def fact(n)
   x = 1
   (1..n).each do |i|
@@ -5,15 +7,15 @@ def fact(n)
   end
   x
 end
+
 def pascal(n)
   (0..n).each do |k|
     yield(fact(n) / (fact(k) * fact(n - k)))
   end
 end
-n= gets.chomp.to_i
-m=n-1
+n = gets.chomp.to_i
+m = n - 1
 (0..m).each do |i|
   pascal(i) { |x| print "#{x} " }
   print "\n"
 end
-
