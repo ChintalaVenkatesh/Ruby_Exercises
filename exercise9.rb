4ec83c7a0b84b36e7ce0d38edc9a44b0f4124833
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:27:25 +0530

    updating

--- exercise9.rb
+++ exercise9.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 input = gets.chomp
 string_array = []
-input.split(" ").each do |i|
-    string_array.push(i)
+input.split(' ').each do |i|
+  string_array.push(i)
 end
-hash_array= Hash.new
+hash_array = {}
 string_array.each_with_object(hash_array) do |x, y|
-    y[x.length] ||= []
-    y[x.length] << x
+  y[x.length] ||= []
+  y[x.length] << x
 end
 
 print hash_array.sort.to_h
