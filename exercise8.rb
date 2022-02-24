5c11577a840c27063bc244e79bab66cd12f75d1b
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:26:41 +0530

    updating

--- exercise8.rb
+++ exercise8.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Array
- def power_of_array(p)
-    self.map {|n| n**p}
- end
+  def power_of_array(p)
+    map { |n| n**p }
+  end
 end
 
 array1 = ARGV[0]
@@ -9,8 +11,6 @@ p = ARGV[1].to_i
 array1 = array1.split(/\D+/).map(&:to_i)
 array1.delete_at(0)
 p array1.power_of_array(p)
-#end
-
 
 
 
