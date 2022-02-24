11e142e263364adca3e4950ce3e32670a554d2aa
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:34:08 +0530

    updating

--- exercise17.rb
+++ exercise17.rb
@@ -1,32 +1,36 @@
-require "time"
-Regex = /^(([0-1]?\d)|(2?[0-3]))(:([0-5]\d)){2}$/    
-def is_valid?(input)
-    Regex =~ input
-end  
-def addition(input_times)
- x = input_times.split(/[\s,\.]+/)
-    if (x.all? { |time| is_valid?(time) })
-     t2 = Time.parse(x[0])
-     sum = x.map { |time| Time.parse(time) }.inject do |total, time|
-     total += time.sec + time.min * 60 + time.hour * 3600
-    end
-     output(sum, t2)
-    else
-     puts "invalid input"
-    end
-end  
-def output(sum, t2)
- hours = sum.hour.to_s.length > 1 ? sum.hour : "0#{sum.hour.to_i}"
- minutes = sum.min.to_s.length > 1 ? sum.min : "0#{sum.min.to_i}"
- seconds = sum.sec.to_s.length > 1 ? sum.sec : "0#{sum.sec.to_i}"
- final = "#{hours}:#{minutes}:#{seconds}"
- day = sum.day - t2.day
- if (day > 0)
-  "#{day} #{day > 1 ? 'days' : 'day'} & #{final}"
- else
-  "#{final}"
- end
-end  
+# frozen_string_literal: true
 
-puts "Enter two time inputs to be added"
+require 'time'
+Regex = /^(([0-1]?\d)|(2?[0-3]))(:([0-5]\d)){2}$/.freeze
+def is_valid?(input)
+  Regex =~ input
+end
+
+def addition(input_times)
+  x = input_times.split(/[\s,.]+/)
+  if x.all? { |time| is_valid?(time) }
+    t2 = Time.parse(x[0])
+    sum = x.map { |time| Time.parse(time) }.inject do |total, time|
+      total += time.sec + time.min * 60 + time.hour * 3600
+    end
+    output(sum, t2)
+  else
+    puts 'invalid input'
+  end
+end
+
+def output(sum, t2)
+  hours = sum.hour.to_s.length > 1 ? sum.hour : "0#{sum.hour.to_i}"
+  minutes = sum.min.to_s.length > 1 ? sum.min : "0#{sum.min.to_i}"
+  seconds = sum.sec.to_s.length > 1 ? sum.sec : "0#{sum.sec.to_i}"
+  final = "#{hours}:#{minutes}:#{seconds}"
+  day = sum.day - t2.day
+  if day.positive?
+    "#{day} #{day > 1 ? 'days' : 'day'} & #{final}"
+  else
+    final.to_s
+  end
+end
+
+puts 'Enter two time inputs to be added'
 puts addition(gets.chomp)
