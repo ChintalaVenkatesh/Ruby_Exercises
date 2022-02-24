1fe0875e257e219b21fa2f2498e3974af9ffd805
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:30:48 +0530

    updating

--- exercise12.rb
+++ exercise12.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
 
-count1=0
-count2=0
-count3=0
-count4=0 
+count1 = 0
+count2 = 0
+count3 = 0
+count4 = 0
 
-puts "Please provide an input"
-line=gets.chomp
-n=line.length
+puts 'Please provide an input'
+line = gets.chomp
+n = line.length
 
-for i in 0..n-1
- if line[i].match?(/[a-z]/)
-    count1+=1
- elsif line[i].match?(/[A-Z]/)
-    count2+=1
- elsif line[i].to_i.to_s==line[i]
-    count3+=1
- else
-    count4+=1
- end
+(0..n - 1).each do |i|
+  case line[i]
+  when /[a-z]/
+    count1 += 1
+  when /[A-Z]/
+    count2 += 1
+  when line[i].to_i.to_s
+    count3 += 1
+  else
+    count4 += 1
+  end
 end
 puts "Lowercase characters = #{count1}
 Uppercase characters = #{count2}
 Numeric characters = #{count3}
 Special characters =#{count4}"
-
