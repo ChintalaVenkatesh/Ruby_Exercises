a524634dbbc8d190acffd01af7cc43d258170e77
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:31:47 +0530

    updating

--- exercise13.rb
+++ exercise13.rb
@@ -1,11 +1,13 @@
-def factorial(n)
-    total = 1
-    (1..n).each do |n|
-      total *= n   
-    end
-    total
-  end
+# frozen_string_literal: true
 
-  puts " please provide an input"
-  input=gets.chomp.to_i
-  puts factorial(input)
+def factorial(n)
+  total = 1
+  (1..n).each do |n|
+    total *= n
+  end
+  total
+end
+
+puts ' please provide an input'
+input = gets.chomp.to_i
+puts factorial(input)
