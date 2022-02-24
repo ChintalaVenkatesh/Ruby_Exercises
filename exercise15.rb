ee0da243881f9a82d48a2e6cd4ddf219f815fd16
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:33:01 +0530

    updating

--- exercise15.rb
+++ exercise15.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 def is_prime?(num)
-    return if num <= 1
-    (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+  return if num <= 1
+
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
-array1=[]
-puts "please provide an input"
-n= gets.chomp.to_i
-0.step(n,1) do |n|
-    if is_prime?(n)==true
-        array1.push(n)
-    end
+array1 = []
+puts 'please provide an input'
+n = gets.chomp.to_i
+0.step(n, 1) do |n|
+  array1.push(n) if is_prime?(n) == true
 end
 print array1
