5990dbdbd3703712664c0dfecfa9d00d64fcdee0
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:36:27 +0530

    updating

--- exercise21.rb
+++ exercise21.rb
@@ -1,13 +1,20 @@
+
+
 class Integer
-    def factorial
-      raise Negativeinput, "Negative number Entered" if self < 0
-      f = 1; for i in 1..self; f *= i; end; f
+  def factorial
+    raise Negativeinput, 'Negative number Entered' if negative?
+
+    f = 1
+    (1..self).each do |i|
+      f *= i
     end
+    f
+  end
 end
 
 class Negativeinput < StandardError
 end
 
-puts "please provide an input"
+puts 'please provide an input'
 input = gets.chomp.to_i
-puts "#{input.factorial}"
+puts input.factorial.to_s
