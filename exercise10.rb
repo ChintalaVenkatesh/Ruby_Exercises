64d5c4941168a53cbd20b69aaff97c21e59a5fa4
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:28:23 +0530

    updating

--- exercise10.rb
+++ exercise10.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 inputString = gets.chomp
 array1 = []
@@ -5,19 +6,22 @@ inputString.split(' ').each do |i|
   array1 << i
 end
 
-group_hash=Hash.new
-array1.inject(group_hash) { |x,y| x[y.length] ||=[]; x[y.length]<<y; x }
+group_hash = {}
+array1.each_with_object(group_hash) do |y, x|
+  x[y.length] ||= []
+  x[y.length] << y
+end
 
-result_hash= Hash.new
+result_hash = {}
 
 group_hash.each do |key, value|
-    if key%2!= 0
-        result_hash['odd'] ||= []
-        result_hash['odd'].push(value)
-    else
-        result_hash['even'] ||=[]
-        result_hash['even'].push(value)
-    end
+  if key.odd?
+    result_hash['odd'] ||= []
+    result_hash['odd'].push(value)
+  else
+    result_hash['even'] ||= []
+    result_hash['even'].push(value)
+  end
 end
 
 puts result_hash
