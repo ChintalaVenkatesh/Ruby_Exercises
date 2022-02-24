ec80963023de177e1dabca2c4c5633ccc3d8c598
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:38:48 +0530

    updating

--- reverse_iterate.rb
+++ reverse_iterate.rb
@@ -1,17 +1,19 @@
-array1 = Array.new()
-array1=[]
-puts"enter array_size"
-i=gets.chomp.to_i
-while i>0
-  puts "Enter next array element"
+# frozen_string_literal: true
+
+array1 = []
+array1 = []
+puts 'enter array_size'
+i = gets.chomp.to_i
+while i.positive?
+  puts 'Enter next array element'
   num = gets.chomp.to_i
   array1.push(num)
-  i = i-1
+  i -= 1
 end
 
 def reverse(array)
- print array.values_at(*((array.size)-1).downto(0))
+  print array.values_at(*(array.size - 1).downto(0))
 end
 print reverse(array1)
 
-#puts "#{array1.reverse}"
+# puts "#{array1.reverse}"
