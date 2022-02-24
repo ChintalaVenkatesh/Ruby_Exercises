7d36d6ba9c5eea9d6ea58e861357baed79d97b9f
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:35:28 +0530

    updating

--- exercise20.rb
+++ exercise20.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 class Interest
-    attr_accessor :principal, :time
-    RATE = 0.1
-    def initialize(principal, time)
-        @principal =principal
-        @time =time
-    end
-    def compound
-        @principal * ((1 + RATE) ** @time).to_f
-    end
-  
-    def simple
-        @principal + (@principal * @time * RATE).to_f
-    end
-  
-    def difference
-       compound-simple
-    end
+  attr_accessor :principal, :time
+
+  RATE = 0.1
+  def initialize(principal, time)
+    @principal = principal
+    @time = time
+  end
+
+  def compound
+    @principal * ((1 + RATE)**@time).to_f
+  end
+
+  def simple
+    @principal + (@principal * @time * RATE).to_f
+  end
+
+  def difference
+    compound - simple
+  end
 end
-puts "Enter Principal "
+puts 'Enter Principal '
 principal = gets.chomp.to_f
-puts "Enter Time"
-time =gets.chomp.to_f
-final_result = Interest.new(principal,time)
-puts "Interest difference:#{(final_result.difference).round(2)}" 
+puts 'Enter Time'
+time = gets.chomp.to_f
+final_result = Interest.new(principal, time)
+puts "Interest difference:#{(final_result.difference).round(2)}"
