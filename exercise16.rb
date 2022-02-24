acc7d3bedb7edef0beb8642f1e35055d02677fd0
Venkatesh <65492179+ChintalaVenkatesh@users.noreply.github.com>
2022-02-24 15:33:34 +0530

    updating

--- exercise16.rb
+++ exercise16.rb
@@ -1,8 +1,11 @@
-print "Please provide an input "
+# frozen_string_literal: true
+
+print 'Please provide an input '
 input = gets.chomp
-puts" please provide word/letter to be highlighted"
+puts ' please provide word/letter to be highlighted'
 highlight_word = gets.chomp
 modified = input.split(' ')
 puts "Total occurences found = #{modified.count(highlight_word)}"
-input=input.gsub(highlight_word,"(#{highlight_word})")
+input = input.gsub(highlight_word, "(#{highlight_word})")
 puts input
+
